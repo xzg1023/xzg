5778607e6feac035e5b5e50d601d380984e08b12
xzg <xzg_1023@163.com>
2020-12-01 20:42:34 +0800

    上传项目测试

--- src/main/java/com/fh/commons/result/CommonsReturn.java
+++ src/main/java/com/fh/commons/result/CommonsReturn.java
@@ -14,7 +14,6 @@ public class CommonsReturn {
     private CommonsReturn(Integer code,String  msg){
         this.code=code;
         this.msg=msg;
-
     }
     private CommonsReturn(Integer code,String  msg,Object data){
         this.code=code;

d5cc50356141a41a61dbf14987285d01e7a6aa54
xzg <xzg_1023@163.com>
2020-12-02 08:07:13 +0800

    上传项目测试3

---
+++ src/main/java/com/fh/config/interceptor/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.fh.config.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @author xzg
+ * @create 2020-12-01 下午 9:16
+ */
+@Configuration
+public class CorsConfig extends WebMvcConfigurationSupport {
+    @Autowired
+    private CorsHandlerInterceptor corsHandlerInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(corsHandlerInterceptor).addPathPatterns("/**");
+    }
+}
